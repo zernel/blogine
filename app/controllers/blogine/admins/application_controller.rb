c2cbcaab318bb77aaebcda01fd6342d3c05461be
Zernel <itzernel@gmail.com>
2018-01-15 20:42:13 +0800

    Added HTTP authentication for admin pages

--- app/controllers/blogine/admins/application_controller.rb
+++ app/controllers/blogine/admins/application_controller.rb
@@ -2,6 +2,8 @@ require_relative '../concerns/select_tab'
 
 module Blogine
   class Admins::ApplicationController < ApplicationController
+    http_basic_authenticate_with name: "admin", password: "password"
+
     include SelectTab
 
     def admins_page?; true; end
