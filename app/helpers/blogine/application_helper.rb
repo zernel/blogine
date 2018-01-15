ff23bc2b60af857dda61415e6815658a102a49f2
Zernel <itzernel@gmail.com>
2018-01-15 09:29:23 +0800

    fixed #2 Added flash notice with bootstrap style

--- app/helpers/blogine/application_helper.rb
+++ app/helpers/blogine/application_helper.rb
@@ -4,5 +4,16 @@ module Blogine
     def all_categories
       @all_categories ||= Blogine::Category.all
     end
+
+    def bootstrap_alert_klass(flash_key)
+      case flash_key
+      when 'notice'
+        return "alert-warning"
+      when 'error'
+        return "alert-danger"
+      else
+        return 'alert-primary'
+      end
+    end
   end
 end
