732d757a658e0d7c1d30cb3fcbb77356b5b71fec
Zernel <itzernel@gmail.com>
2018-01-18 23:57:06 +0800

    Added admins layout, adjust the style for pages

--- app/controllers/blogine/admins/application_controller.rb
+++ app/controllers/blogine/admins/application_controller.rb
@@ -4,6 +4,8 @@ module Blogine
   class Admins::ApplicationController < ApplicationController
     http_basic_authenticate_with name: "admin", password: "password"
 
+    layout 'blogine/admins_application'
+
     include SelectTab
 
     def admins_page?; true; end
